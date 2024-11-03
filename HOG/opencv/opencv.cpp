011e5119c2627791f4c32b482f703a9af7434621
재민 최 <choijm0007@gmail.com>
2024-11-03 18:02:46 +0900

    abs을 이용한 HOG

--- HOG/opencv/opencv.cpp
+++ HOG/opencv/opencv.cpp
@@ -2,10 +2,10 @@
 #include <opencv2/opencv.hpp>
 #include <cmath>
 
-#define WINDOWSIZE_WIDTH 64
-#define WINDOWSIZE_HEIGHT 128
-#define CELLSIZE 8
-#define BLOCKSIZE 16
+#define WINDOWSIZE_WIDTH 512
+#define WINDOWSIZE_HEIGHT 512
+#define CELLSIZE 16
+#define BLOCKSIZE 32
 
 using namespace cv;
 
@@ -16,11 +16,11 @@ int op();
 void GetHistogram(const Mat& mag, const Mat& phase, std::vector<float>& hist);
 void getBlock(const Mat& mag, const Mat& phase, int x, int y, std::vector<float>& hist);
 float* getCell(const Mat& mag, const Mat& phase, int x, int y);
-
+Mat visualizeHOG(const std::vector<float>& featureVector, int cellSize, int binSize, int width, int height);
 
 int main() {
     Mat t = imread("lena.jpg", IMREAD_GRAYSCALE);
-    Mat scr;
+    Mat scr = t.clone();
     resize(t, scr, Size(WINDOWSIZE_WIDTH, WINDOWSIZE_HEIGHT));
     if (scr.empty()) {
         std::cerr << "Error loading image!" << std::endl;
@@ -44,13 +44,7 @@ int main() {
     Mat phase(scr.size(), CV_32F);
 
     getMagPhase(x, y, mag, phase);
-    double minVal, maxVal;
-    Point minLoc, maxLoc;
 
-    // 최솟값과 최댓값 계산
-    minMaxLoc(phase, &minVal, &maxVal, &minLoc, &maxLoc);
-    std::cout << "최솟값: " << minVal << ", 위치: " << minLoc << std::endl;
-    std::cout << "최댓값: " << maxVal << ", 위치: " << maxLoc << std::endl;
    
     //mag.convertTo(mag, CV_8UC1);
     //imshow("Magnitude", mag);
@@ -59,29 +53,105 @@ int main() {
     GetHistogram(mag, phase, histogram);
     std::cout << std::endl <<std::endl<< histogram.size() << std::endl << std::endl;
 
-    imshow("input", t);
-    imshow("scr", scr);
+
+
+    //imshow("input", t);
+    //imshow("scr", scr);
     
-    //op();
+    Mat hogImage = visualizeHOG(histogram, CELLSIZE, 9, WINDOWSIZE_WIDTH, WINDOWSIZE_HEIGHT);
+    Mat combine;
+    addWeighted(scr, 1.0, hogImage, 0.2, 0, combine);
+    imshow("combine",combine);
+
+
+    HOGDescriptor hog(
+        Size(WINDOWSIZE_WIDTH, WINDOWSIZE_HEIGHT),   // 윈도우 사이즈
+        Size(BLOCKSIZE, BLOCKSIZE),    // 블록 사이즈
+        Size(CELLSIZE, CELLSIZE),      // 블록 스트라이드
+        Size(CELLSIZE, CELLSIZE),      // 셀 사이즈
+        9                // 빈의 수 (각 히스토그램 방향 개수)
+    );
+    std::vector<float> descriptors;
+    hog.compute(scr, descriptors, Size(CELLSIZE, CELLSIZE));
+    
+
+    Mat hogImage2 = visualizeHOG(descriptors, CELLSIZE, 9, WINDOWSIZE_WIDTH, WINDOWSIZE_HEIGHT);
+    Mat combine2;
+    addWeighted(scr, 1.0, hogImage2, 0.2, 0, combine2);
+    imshow("opencv", combine2);
+
+    std::cout << std::endl << "mine, opencv\n";
+    for (int i = 0; i < 50; i++) {
+        std::cout << histogram[i] << ", ";
+        std::cout << descriptors[i] << std::endl;
+    }
+
+
     waitKey(0);
     return 0;
 }
 
+
+
+
+// HOG 특징 벡터를 시각화하는 함수
+Mat visualizeHOG(const std::vector<float>& featureVector, int cellSize, int binSize, int width, int height) {
+    int numCellsX = width / cellSize;
+    int numCellsY = height / cellSize;
+    int angleUnit = 360 / binSize;
+
+    // 시각화 이미지 생성
+    Mat hogImage(height, width, CV_8UC1, Scalar(255, 255, 255));
+
+    int index = 0;
+    for (int y = 0; y < numCellsY; y++) {
+        for (int x = 0; x < numCellsX; x++) {
+            // 현재 셀의 히스토그램 가져오기
+            std::vector<float> hist(binSize);
+            for (int i = 0; i < binSize; i++) {
+                hist[i] = featureVector[index++];
+            }
+
+            // 셀의 중심 좌표
+            Point cellCenter(x * cellSize + cellSize / 2, y * cellSize + cellSize / 2);
+
+            // 히스토그램을 기반으로 선 그리기
+            for (int bin = 0; bin < binSize; bin++) {
+                float magnitude = hist[bin];
+                float angle = bin * angleUnit * CV_PI / 180.0; // 라디안 단위로 변환
+
+                // 선의 시작점과 끝점 계산
+                Point startPoint(cellCenter.x + cos(angle) * magnitude * cellSize / 2,
+                    cellCenter.y + sin(angle) * magnitude * cellSize / 2);
+                Point endPoint(cellCenter.x - cos(angle) * magnitude * cellSize / 2,
+                    cellCenter.y - sin(angle) * magnitude * cellSize / 2);
+
+                // 선 그리기
+                line(hogImage, startPoint, endPoint, Scalar(0, 0, 255), 1);
+            }
+        }
+    }
+
+    // 시각화된 HOG 이미지 출력
+    //imshow("HOG Visualization", hogImage);
+
+    return hogImage;
+}
+
+
 void GetHistogram(const Mat& mag, const Mat& phase, std::vector<float>& hist) {
     int height = mag.rows;
     int width = mag.cols;
-    int cnt1 = 0;
+
     for (int h = 0; h < height-CELLSIZE; h += CELLSIZE){
-        cnt1++;
-        int cnt2 = 0;
+
         for (int w = 0; w < width-CELLSIZE; w += CELLSIZE) {
             getBlock(mag, phase, w, h, hist);
-            cnt2++;
+
         }
-        std::cout << std::endl << "cnt2 갯수" << cnt2 << std::endl;
+
     }
-    std::cout << std::endl << "cnt1 갯수" << cnt1 << std::endl;
-    //128 -> 105
+
 }
 
 void getBlock(const Mat& mag, const Mat& phase, int x, int y, std::vector<float>& hist) {
@@ -89,31 +159,33 @@ void getBlock(const Mat& mag, const Mat& phase, int x, int y, std::vector<float>
     float* tmp;
     std::vector<float> L;
     float total;
-    
+    total = 0;
     for (int h = 0; h < BLOCKSIZE; h += CELLSIZE) for (int w = 0; w < BLOCKSIZE; w += CELLSIZE) {
-        tmp = getCell(mag,phase,x+w, y+h);
-        total = 0;
+        tmp = getCell(mag, phase, x + w, y + h);
+        
         // 정규화 과정
         for (int i = 0; i < 9; i++) {
-            total = total + tmp[i]*tmp[i];
+            total = total + tmp[i] * tmp[i];
             L.push_back(tmp[i]);
         }
-        total = sqrt(total);
-        for (int i = 0; i < 9; i++) {
-            hist.push_back(L[i] / total);
-        }
-        
+        delete[] tmp;
     }
+    total = sqrt(total);
+    for (int i = 0; i < L.size(); i++) {
+        hist.push_back(L[i] / total);
+    }
+    
+    
     
 }
 
 float* getCell(const Mat& mag,const Mat& phase, int x, int y) {
 
-    float arr[9] = { 0 };
+    float* arr = new float[9]();
     int i1,i2;
     float di;
     float t;
-    for (int h = y; h < CELLSIZE; h++) for (int w = x; w < CELLSIZE; w++) {
+    for (int h = y; h < y+CELLSIZE; h++) for (int w = x; w < x+CELLSIZE; w++) {
         t = phase.at<float>(h, w);
         i1 = t / 20;
         di = t - i1*20;
