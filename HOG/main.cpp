e501b873d3bfc485f2bc7e48e1f0c8abbbfeb621
이준호 <jglee40@naver.com>
2024-11-06 21:40:21 +0900

    normalize 단계 오류 수정

--- HOG/main.cpp
+++ HOG/main.cpp
@@ -1,11 +1,10 @@
 ﻿#include <opencv2/opencv.hpp>
 
 #include <cmath>
-#include <vector>
 
-#define PI	(3.14159265358979323846)
-
+//#define SHOW_PADDING
 //#define SHOW_GRADIENT
+//#define COUT_NORMALIZE_VALUE_RANGE
 
 using namespace cv;
 
@@ -14,16 +13,45 @@ void gradient(const Mat& in, Mat& out, int padSize);
 void histogramGradient(const Mat& in, Mat& out, int cellSize);
 void histNormalize(Mat& in, Mat& out);
 
+// 데카르트 좌표계 실험
+void polarToCartesian(Mat& in, Mat& out);
+Mat gammaCorrection(const cv::Mat& input, double gamma);
+
 int main() {
 	Mat image = imread("Lena.png");
+	Mat resize_image = image.clone();
+	//resize(image, resize_image, Size(64, 128));
 	Mat padImage, gradientImage, histImage, normalizeHistImage;
 
-	padding(image, padImage, 1);
+	padding(resize_image, padImage, 1);
 	gradient(padImage, gradientImage, 1);
 	histogramGradient(gradientImage, histImage, 8);
 	histNormalize(histImage, normalizeHistImage);
 
-	imshow("img", image);
+#ifdef COUT_NORMALIZE_VALUE_RANGE
+	float minValue = 1, maxValue = 0;
+	for (int row = 0; row < normalizeHistImage.rows; row++) {
+		float* ptr = normalizeHistImage.ptr<float>(row);
+		for (int col = 0; col < normalizeHistImage.cols; col++) {
+			for (int i = 0; i < 9; i++) {
+				if (ptr[col * 9 + i] <= 0) {
+					std::cout << "row: " << row << "\tcol: " << col << "\tchannel: " << i << "\tvalue: " << ptr[col * 9 + i] << '\n';
+				} else if (ptr[col * 9 + i] >= 1) {
+					std::cout << "row: " << row << "\tcol: " << col << "\tchannel: " << i << "\tvalue: " << ptr[col * 9 + i] << '\n';
+				}
+				minValue = min(minValue, ptr[col * 9 + i]);
+				maxValue = max(maxValue, ptr[col * 9 + i]);
+			}
+		}
+	}
+	std::cout << minValue << ' ' << maxValue << "\n\n";
+#endif // COUT_NORMALIZE_VALUE_RANGE
+
+	//Mat cartesianImage;
+
+	//polarToCartesian(gradientImage, cartesianImage);
+
+	//imshow("img", image);
 
 	waitKey();
 
@@ -63,6 +91,12 @@ void padding(const Mat& in, Mat& out, int padSize) {
 			}
 		}
 	}
+
+#ifdef SHOW_PADDING
+	imshow("padding", out);
+	waitKey();
+	destroyAllWindows();
+#endif // SHOW_PADDING
 }
 
 void gradient(const Mat& in, Mat& out, int padSize) {
@@ -93,43 +127,76 @@ void gradient(const Mat& in, Mat& out, int padSize) {
 				gy.data[channel * (y * gy.cols + x) + c] = gY;
 
 				magnitude.data[channel * (y * magnitude.cols + x) + c] = (uchar)sqrt(gX * gX + gY * gY);
-				degree = atan2(gY, gX) * 180 / PI;
+				degree = atan2(gY, gX) * 180 / CV_PI;
 				angle.data[channel * (y * angle.cols + x) + c] = (unsigned char)(degree < 0 ? degree + 180 : degree) % 180;
 			}
 		}
 	}
 
-	// 채널 개수 생각
+	int* pixelValue = new int[channel];
+	int cIndex, maxValue;
+
 	for (int y = 0; y < row; y++) {
 		for (int x = 0; x < col; x++) {
-			int c = -1;
-			int b = magnitude.data[channel * (y * magnitude.cols + x) + 0];
-			int g = magnitude.data[channel * (y * magnitude.cols + x) + 1];
-			int r = magnitude.data[channel * (y * magnitude.cols + x) + 2];
+			cIndex = -1;
+			maxValue = -1;
 
-			if (b > g) {
-				if (b > r) { // b
-					c = 0;
-				} else { // r
-					c = 2;
-				}
-			} else {
-				if (g > r) { // g
-					c = 1;
-				} else { // r
-					c = 2;
+			for (int c = 0; c < channel; c++) {
+				pixelValue[c] = magnitude.data[channel * (y * magnitude.cols + x) + c];
+			}
+
+			for (int c = 0; c < channel; c++) {
+				if (pixelValue[c] > maxValue) {
+					maxValue = pixelValue[c];
+					cIndex = c;
 				}
 			}
 
-			if (c == -1) {
+			if (cIndex == -1 || maxValue == -1) {
 				std::cerr << "gradient - max magnitude value is not found\n";
 				return;
 			}
 
-			out.data[2 * (y * out.cols + x) + 0] = magnitude.data[channel * (y * magnitude.cols + x) + c];
-			out.data[2 * (y * out.cols + x) + 1] = angle.data[channel * (y * angle.cols + x) + c];
+			out.data[2 * (y * out.cols + x) + 0] = magnitude.data[channel * (y * magnitude.cols + x) + cIndex];
+			out.data[2 * (y * out.cols + x) + 1] = angle.data[channel * (y * angle.cols + x) + cIndex];
 		}
 	}
+
+	delete[] pixelValue;
+
+#pragma region deprecated_colorGradient
+	// 컬러 영상에서만 작동
+	//for (int y = 0; y < row; y++) {
+	//	for (int x = 0; x < col; x++) {
+	//		int c = -1;
+	//		int b = magnitude.data[channel * (y * magnitude.cols + x) + 0];
+	//		int g = magnitude.data[channel * (y * magnitude.cols + x) + 1];
+	//		int r = magnitude.data[channel * (y * magnitude.cols + x) + 2];
+
+	//		if (b > g) {
+	//			if (b > r) { // b
+	//				c = 0;
+	//			} else { // r
+	//				c = 2;
+	//			}
+	//		} else {
+	//			if (g > r) { // g
+	//				c = 1;
+	//			} else { // r
+	//				c = 2;
+	//			}
+	//		}
+
+	//		if (c == -1) {
+	//			std::cerr << "gradient - max magnitude value is not found\n";
+	//			return;
+	//		}
+
+	//		out.data[2 * (y * out.cols + x) + 0] = magnitude.data[channel * (y * magnitude.cols + x) + c];
+	//		out.data[2 * (y * out.cols + x) + 1] = angle.data[channel * (y * angle.cols + x) + c];
+	//	}
+	//}
+#pragma endregion
 
 #ifdef SHOW_GRADIENT
 	imshow("gx", gx);
@@ -137,6 +204,7 @@ void gradient(const Mat& in, Mat& out, int padSize) {
 	imshow("magnitude", magnitude);
 	imshow("angle", angle);
 	waitKey();
+	destroyAllWindows();
 #endif // SHOW_GRADIENT
 }
 
@@ -207,16 +275,19 @@ void histNormalize(Mat& in, Mat& out) {
 				sum += histogram[i] * histogram[i];
 			}
 
+			if (sum == 0) {
+				std::cerr << "histNormalization - sum value error\n";
+				return;
+			}
+
 			float norm = sqrt(sum);
 
 			float* outPtr1 = out.ptr<float>(row);
 			float* outPtr2 = out.ptr<float>(row + 1);
 
-			for (int i = 0; i < 9; i++) {
+			for (int i = 0; i < 18; i++) {
 				outPtr1[col * 9 + i] += histogram[i] / norm;
-				outPtr1[col * 9 + i + 9] += histogram[i + 9] / norm;
 				outPtr2[col * 9 + i] += histogram[i + 18] / norm;
-				outPtr2[col * 9 + i + 9] += histogram[i + 27] / norm;
 			}
 		}
 	}
@@ -226,19 +297,84 @@ void histNormalize(Mat& in, Mat& out) {
 
 		for (int col = 0; col < out.cols; col++) {
 			if ((row == 0 || row == out.rows - 1) && (col == 0 || col == out.cols - 1)) {
-				break;
+				continue; // 꼭짓점
 			} else if (row == 0 || row == out.rows - 1 || col == 0 || col == out.cols - 1) {
 				for (int i = 0; i < 9; i++) {
-					outPtr[col * 9 + i] /= 2;
+					outPtr[col * 9 + i] /= 2; // 가장자리
 				}
 			} else {
 				for (int i = 0; i < 9; i++) {
-					outPtr[col * 9 + i] /= 4;
+					outPtr[col * 9 + i] /= 4; // 내부
 				}
 			}
 		}
 	}
 }
 
+void polarToCartesian(Mat& in, Mat& out) {
+	if (in.type() != CV_8UC2) {
+		std::cerr << "polarToCartesian - input Mat type error\n";
+		return;
+	}
+
+	int rows = in.rows, cols = in.cols, channel = in.channels();
+	float xMax = 0, yMax = 0; // test
+
+	out = Mat::zeros(in.rows, in.cols, CV_32FC2);
+
+	for (int row = 0; row < rows; row++) {
+		float* outPtr = out.ptr<float>(row);
+
+		for (int col = 0; col < cols; col++) {
+			int pixelMag = in.data[channel * (row * in.cols + col) + 0];
+			int pixelAngle = in.data[channel * (row * in.cols + col) + 1];
+
+			outPtr[col * channel + 0] = pixelMag * cos(pixelAngle); // x
+			outPtr[col * channel + 1] = pixelMag * sin(pixelAngle); // y
+
+			xMax = max(abs(outPtr[col * channel + 0]), xMax);
+			yMax = max(abs(outPtr[col * channel + 1]), yMax);
+		}
+	}
+
+	// test - 시각화
+
+	xMax = floor(xMax + 0.5);
+	yMax = floor(yMax + 0.5);
+
+	Mat plane = Mat::zeros(2 * yMax + 2, 2 * xMax + 2, CV_8UC1);
+
+	for (int row = 0; row < rows; row++) {
+		float* outPtr = out.ptr<float>(row);
+
+		for (int col = 0; col < cols; col++) {
+			int xPixel = floor(outPtr[col * channel + 0] + 0.5); // x
+			int yPixel = floor(outPtr[col * channel + 1] + 0.5); // y
+
+			xPixel += plane.cols / 2;
+			yPixel += plane.rows / 2;
+
+			uchar* pixelPtr = plane.ptr<uchar>(plane.rows - yPixel);
+			pixelPtr[xPixel]++;
+		}
+	}
+
+	Mat planeGamma = gammaCorrection(plane, 0.1);
+
+	imshow("plane", plane);
+	imshow("gamma", planeGamma);
+}
+
+Mat gammaCorrection(const cv::Mat& input, double gamma) {
+	// Look Up Table 생성
+	cv::Mat lut(1, 256, CV_8UC1);
+	for (int i = 0; i < 256; ++i) {
+		lut.at<uchar>(i) = cv::saturate_cast<uchar>(pow(i / 255.0, gamma) * 255.0);
+	}
+
+	cv::Mat output;
+	cv::LUT(input, lut, output); // LUT을 사용하여 변환
+	return output;
+}
+
 // Mat 타입
-// 정규화 할때 중복되는 부분
